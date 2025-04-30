50f7a4d639a7f98f46d4bcb56668dbb7efc877f8
lesley <68919438+lesley78@users.noreply.github.com>
2025-04-30 14:50:23 +0300

    Лабораторная7

---
+++ wpf_template/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CustomButtonTemplate
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
